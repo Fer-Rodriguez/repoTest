637cac214e4e5aa8dbe1c834607daf9322dfbd56
Fernando De La Torre <ref2k1@gmail.com>
2022-02-19 12:50:33 -0600

    funcion numerica

---
+++ funciones.py
@@ -0,0 +1,5 @@
+# esta funcion imprime numeros
+def propiedad_variable_integer(valor_numerico):
+    return print("El valor numerico es: ", valor_numerico)
+
+propiedad_variable_integer(20)
--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
 print("Hello world")
+
+var1=20
+var2=30
+
+var3=0
+
+print(var1+var2)
